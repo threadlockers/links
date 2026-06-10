da73acde0ab33f374b016d737b133d97bcfbc1d7
Mukesh <hey.mukesh272921@gmail.com>
2026-06-11 02:17:31 +0530

    bot: replace link emoji with checkmark emoji on successful insert to linkding

--- core/bot/bot.go
+++ core/bot/bot.go
@@ -17,6 +17,8 @@ type Bot struct {
 }
 
 var TWITTER_HOSTS = []string{"x.com", "twitter.com", "www.x.com", "www.twitter.com"}
+var LINK_EMOJI = "🔗"
+var CHECKMARK_EMOJI = "✅"
 
 func New(cfg config.EnvCfg) (*Bot, error) {
 	session, err := discordgo.New("Bot " + cfg.DiscordBotToken)
@@ -92,9 +94,6 @@ func (b *Bot) onMessageReactionAdd(s *discordgo.Session, r *discordgo.MessageRea
 	description := ""
 	poster := msg.Author.Username
 
-	if strings.Contains(url.String(), "arxiv.org/pdf") {
-	}
-
 	if slices.Contains(TWITTER_HOSTS, url.Host) {
 		title, description, err = utils.GetTitleAndDescriptionForTweet(url)
 		if err != nil {
@@ -112,7 +111,6 @@ func (b *Bot) onMessageReactionAdd(s *discordgo.Session, r *discordgo.MessageRea
 	}
 
 	if title == "" {
-
 		title, err = utils.GetPageTitle(url.String())
 		if err != nil || title == "" {
 			log.Printf("failed to extract title of the url, falling back to url: %s", url)
@@ -125,6 +123,16 @@ func (b *Bot) onMessageReactionAdd(s *discordgo.Session, r *discordgo.MessageRea
 		ApiToken:   b.cfg.LinkdingApiToken,
 	}, url.String(), title, description, poster, remaining); err != nil {
 		log.Printf("failed to add to linkding: %s", err)
+		return
+	}
+
+	if err := b.session.MessageReactionRemove(r.ChannelID, r.MessageID, LINK_EMOJI, r.UserID); err != nil {
+		log.Printf("failed to remove link emoji from message %s: %s", r.MessageID, err)
+		return
+	}
+
+	if err := b.session.MessageReactionAdd(r.ChannelID, r.MessageID, CHECKMARK_EMOJI); err != nil {
+		log.Printf("failed to add checkmark emoji to message %s: %s", r.MessageID, err)
 		return
 	}
 }
